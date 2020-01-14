6463aa7ebd876993d3f762a1af928dc731f251d5
Mirco Zanzaro <m.zanzaro@rch-group.com>
2020-01-14 17:31:39 +0100

    first integration viewModel and list

--- app/src/main/java/com/mz/spendingmanager/list/content/ContentFragment.kt
+++ app/src/main/java/com/mz/spendingmanager/list/content/ContentFragment.kt
@@ -1,13 +1,16 @@
 package com.mz.spendingmanager.list.content
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.mz.spendingmanager.R
+import com.mz.spendingmanager.model.Expense
+import kotlinx.android.synthetic.main.list_fragment.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ContentFragment : Fragment() {
 
@@ -27,7 +30,21 @@ class ContentFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ContentViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.listExpense.observe(this, androidx.lifecycle.Observer {
+            tv_num_elements.text = it.count().toString()
+        })
+
+        fab_create_new_item.setOnClickListener {
+            val id : Long = (viewModel.getListExpense().count() + 1).toLong()
+            val item = Expense(id, "Text item $id", Date())
+
+            val newList = ArrayList<Expense>()
+            newList.addAll(viewModel.getListExpense())
+
+            newList.add(1,item)
+            viewModel.setListExpense(newList)
+        }
     }
 
 }
--- app/src/main/java/com/mz/spendingmanager/list/content/ContentViewModel.kt
+++ app/src/main/java/com/mz/spendingmanager/list/content/ContentViewModel.kt
@@ -1,7 +1,21 @@
 package com.mz.spendingmanager.list.content
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mz.spendingmanager.model.Expense
 
 class ContentViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val listExpense : MutableLiveData<List<Expense>> = MutableLiveData()
+
+    fun setListExpense(result: List<Expense>) {
+        listExpense.value = result
+    }
+
+    fun getListExpense(): List<Expense> {
+        //return current display state or empty string if value is null
+        //see "Elvis Operator"
+        return listExpense.value ?: emptyList()
+    }
+
 }
